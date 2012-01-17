7319ae1bb0b3f977b071dde9b0900892980de257
dotnetwise <laurentiu.macovei@dotnetwise.com>
2012-01-17 16:40:58 +0200

    Added all the overload methods, for both RouteCollection and Areas (AreaRegistrationContext)

--- 2011-12-25 - Clients-side routing with ASP.NET MVC/ClientSideRouting/JavaScriptResult.cs
+++ 2011-12-25 - Clients-side routing with ASP.NET MVC/ClientSideRouting/JavaScriptResult.cs
@@ -1,23 +1,86 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Routing;
 using System.Web.Mvc;
-using System.Text;
 
-namespace ClientSideRouting
+namespace System.Web.Mvc
 {
-    public class JavaScriptResult : ContentResult
+    public static class RoutingExtension
     {
-        public JavaScriptResult(StringBuilder js)
-            : this(js.ToString())
+    	public static Route AddRoute(this RouteCollection routes, string name, string url)
+		{
+			Route route = routes.MapRoute(name, url);
+            route.DataTokens.Add("RouteName", name);
+			return route;
+		}
+		public static Route AddRoute(this RouteCollection routes, string name, string url, object defaults)
         {
+            Route route = routes.MapRoute(name, url, defaults);
+            route.DataTokens.Add("RouteName", name);
+			return route;
+        }
+		public static Route AddRoute(this RouteCollection routes, string name, string url, string[] namespaces)
+		{
+            Route route = routes.MapRoute(name, url, namespaces);
+            route.DataTokens.Add("RouteName", name);
+			return route;
+        }
+		public static Route AddRoute(this RouteCollection routes, string name, string url, object defaults, object constraints)
+		{
+            Route route = routes.MapRoute(name, url, defaults, constraints);
+            route.DataTokens.Add("RouteName", name);
+			return route;
+        }
+		public static Route AddRoute(this RouteCollection routes, string name, string url, object defaults, string[] namespaces)
+		{
+            Route route = routes.MapRoute(name, url, defaults, namespaces);
+            route.DataTokens.Add("RouteName", name);
+			return route;
+        }
+		public static Route AddRoute(this RouteCollection routes, string name, string url, object defaults, object constraints, string[] namespaces)
+		{
+            Route route = routes.MapRoute(name, url, defaults, constraints, namespaces);
+            route.DataTokens.Add("RouteName", name);
+			return route;
         }
 
-        public JavaScriptResult(string js)
-        {
-            this.Content = js;
-            this.ContentType = "text/javascript";
-        }
+		public static Route AddRoute(this AreaRegistrationContext context, string name, string url)
+		{
+			Route route = context.MapRoute(name, url);
+			route.DataTokens.Add("RouteName", name);
+			return route;
+		}
+		public static Route AddRoute(this AreaRegistrationContext context, string name, string url, object defaults)
+		{
+			Route route = context.MapRoute(name, url, defaults);
+			route.DataTokens.Add("RouteName", name);
+			return route;
+		}
+		public static Route AddRoute(this AreaRegistrationContext context, string name, string url, string[] namespaces)
+		{
+			Route route = context.MapRoute(name, url, namespaces);
+			route.DataTokens.Add("RouteName", name);
+			return route;
+		}
+		public static Route AddRoute(this AreaRegistrationContext context, string name, string url, object defaults, object constraints)
+		{
+			Route route = context.MapRoute(name, url, defaults, constraints);
+			route.DataTokens.Add("RouteName", name);
+			return route;
+		}
+		public static Route AddRoute(this AreaRegistrationContext context, string name, string url, object defaults, string[] namespaces)
+		{
+			Route route = context.MapRoute(name, url, defaults, namespaces);
+			route.DataTokens.Add("RouteName", name);
+			return route;
+		}
+		public static Route AddRoute(this AreaRegistrationContext context, string name, string url, object defaults, object constraints, string[] namespaces)
+		{
+			Route route = context.MapRoute(name, url, defaults, constraints, namespaces);
+			route.DataTokens.Add("RouteName", name);
+			return route;
+		}
     }
 }
